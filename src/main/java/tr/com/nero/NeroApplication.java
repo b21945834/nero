e0d41efb7d35409eb7e172a2d7127f85d6f78446
kadiraydogan <72974134+b21945834@users.noreply.github.com>
2024-07-18 13:35:27 +0300

    first commit

---
+++ src/main/java/tr/com/nero/NeroApplication.java
@@ -0,0 +1,13 @@
+package tr.com.nero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/com/nero/NeroApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.com.nero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
